651b2148cd1583ba55fe2b776bcfd47e76b78357
Ronaldo Rodrigues <74150316+Rona1f@users.noreply.github.com>
2020-11-30 22:29:09 -0300

    Update settings.py

--- HOMELEDPROJECT/settings.py
+++ HOMELEDPROJECT/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'c%dv!xa5+7m(h8t&qs1#tq_jl44@%%jgo-hwhyfu4#y(4ia#t+'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
